6740a80257bea0a23bc4ba0c04b2f2acc9a9168e
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-02-15 23:57:32 +0700

    - initial Project.

---
+++ SegmentedControlSwitchPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentedControlSwitchPage
+//
+//  Created by Kittitat Rodphotong on 2/15/2560 BE.
+//  Copyright © 2560 FameSprinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

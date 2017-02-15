b0eaca9ef48392b968d7dccd4ffa2f7f91f8e945
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-02-16 00:00:35 +0700

    - Create `Segmented`.

--- SegmentedControlSwitchPage/ViewController.swift
+++ SegmentedControlSwitchPage/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlet
+    @IBOutlet weak var segmented: UISegmentedControl!
+    
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
